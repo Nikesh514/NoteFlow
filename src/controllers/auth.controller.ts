c8a4b2c0ffd44eecf2b6cfff86dd520b0d2c11ee
Nikesh Mainali <nikeshmainali10@gmail.com>
2025-07-15 21:09:44 +0545

    updated auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express';
 import { hash } from "../utils/bcrypt.utils"
+import CustomError from '../middleware/errorhandler.middleware';
+import User from '../models/user.model';
 
 export const register = async (req: Request, res:Response) => {
     const { full_name,email,password } = req.body;
 
     if (!password) {
-        throw new Error("Password is required",);
+        throw new CustomError("Password is required",400);
     }
 
     const hashPassword = await hash(password)
     
+    // creating new user
+    const user = await User.create({
+        email,
+        full_name,
+        password: hashPassword
+    })
 }
